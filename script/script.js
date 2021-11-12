ff497201f356290c59d72962e8b9093e267343fa
douglas-devinhouse <douglas_nascimento4@estudante.sesisenai.org.br>
2021-11-11 21:13:46 -0300

    Refatoração excluirTarefa, inclusão concluirTarefa

--- script/script.js
+++ script/script.js
@@ -1,12 +1,13 @@
 const LISTA_TAREFAS = [];
 
 function adcionarTarefa(){
-    if (document.querySelector("#inputTarefa").value == "") {
+    let valorInputTarefa = document.querySelector("#inputTarefa").value;
+    if (valorInputTarefa == "") {
       alert("Informe a descrição da tarefa para incluir!");
     } else {
       const objetoTarefa = {
         id: LISTA_TAREFAS.length,
-        textoTarefa: document.querySelector("#inputTarefa").value,
+        textoTarefa: valorInputTarefa,
         concluido: false,        
       };
       
@@ -15,13 +16,23 @@ function adcionarTarefa(){
     }
 }
 
-function deletarTarefa(indice) {
-    LISTA_TAREFAS.splice(
-        //Praticando arrow function: encontre o item => se o id dele for igual ao parâmetro
-        LISTA_TAREFAS.findIndex((item) => item.id == indice),1);
+function excluirTarefa(idTarefa) {
+    //Praticando arrow function: encontre o item => se o id dele for igual ao parâmetro
+    let indiceExcluir = LISTA_TAREFAS.findIndex((item) => item.id == idTarefa);
+    LISTA_TAREFAS.splice(indiceExcluir,1);
         
-        console.log(LISTA_TAREFAS);
+    console.log(LISTA_TAREFAS);
 }
+
+function concluirTarefa(idTarefa) {
+    let indiceConcluir = LISTA_TAREFAS.findIndex((item) => item.id == idTarefa);
+  
+    LISTA_TAREFAS[indiceConcluir].concluido
+        ? (LISTA_TAREFAS[indiceConcluir].concluido = false)
+        : (LISTA_TAREFAS[indiceConcluir].concluido = true);
+    
+    console.log(LISTA_TAREFAS);
+  }
 
 // pretendo incluir uma escuta para incluir na lista ao pressionar enter tbm (pesquisar como fazer isso)
 document.querySelector("#btn-adicionar").addEventListener("click", adcionarTarefa);

36d538b23a411e1e836bcbeb5fbaddd4c37b7737
douglas-devinhouse <douglas_nascimento4@estudante.sesisenai.org.br>
2021-11-12 21:12:22 -0300

    Refatoração e Implementação: remover item da lista

--- script/script.js
+++ script/script.js
@@ -1,5 +1,7 @@
 const LISTA_TAREFAS = [];
 
+const ELEMENTO_Ul = document.querySelector('#lista-tarefas');
+
 function adcionarTarefa(){
     let valorInputTarefa = document.querySelector("#inputTarefa").value;
     if (valorInputTarefa == "") {
@@ -18,11 +20,11 @@ function adcionarTarefa(){
       //Criar uma propriedade "id" para cada novo item da lista (será o index da LISTA_TAREFAS)
       NOVO_ELEMENTO_LI.id = `item-${objetoTarefa.id}`;
       //Adicionar os elementos html na DOM
-      NOVO_ELEMENTO_LI.innerHTML = `<input type="checkbox" name="chk-${objetoTarefa.id}" onclick="marcaFeito(${objetoTarefa.id})">
+      NOVO_ELEMENTO_LI.innerHTML = `<input type="checkbox" name="chk-${objetoTarefa.id}" id="chk-${objetoTarefa.id}" onclick="alterarStatusTarefa(${objetoTarefa.id})">
                                     <label for="chk-${objetoTarefa.id}">${objetoTarefa.textoTarefa}</label>
-                                    <button onclick="removeItem(${objetoTarefa.id})">&times;</button>`;
-      let elementoUl = document.querySelector('#lista-tarefas');
-      elementoUl.appendChild(NOVO_ELEMENTO_LI);
+                                    <button onclick="excluirTarefa(${objetoTarefa.id})">&times;</button>`;      
+      
+      ELEMENTO_Ul.appendChild(NOVO_ELEMENTO_LI);
 
       console.log(LISTA_TAREFAS);
 
@@ -34,14 +36,17 @@ function excluirTarefa(idTarefa) {
     //Praticando arrow function: encontre o item => se o id dele for igual ao parâmetro
     let indiceExcluir = LISTA_TAREFAS.findIndex((item) => item.id == idTarefa);
     LISTA_TAREFAS.splice(indiceExcluir,1);
+
+    let elementoExcluir = document.querySelector(`#item-${idTarefa}`);
+    ELEMENTO_Ul.removeChild(elementoExcluir);
         
     console.log(LISTA_TAREFAS);
 }
 
 function alterarStatusTarefa(idTarefa) {
     let indiceAlterarStatus = LISTA_TAREFAS.findIndex((item) => item.id == idTarefa);    
-    LISTA_TAREFAS[indiceAlterarStatus].concluido = !(LISTA_TAREFAS[indiceAlterarStatus].concluido);    
-    
+    LISTA_TAREFAS[indiceAlterarStatus].concluido = !(LISTA_TAREFAS[indiceAlterarStatus].concluido);        
+       
     console.log(LISTA_TAREFAS);
   }
 
